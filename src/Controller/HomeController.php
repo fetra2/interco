c9997069a1281055185d9ad502bad73e87600a08
fetra <rasolonjatovofetra@gmail.com>
2021-05-27 01:52:07 +0300

    27052021

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+   namespace App\Controller;
+
+   use Symfony\Component\Routing\Annotation\Route;
+   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+   use Symfony\Component\HttpFoundation\Response;
+   use Twig\Environment;
+
+   class HomeController extends AbstractController{
+      /**
+       * @Route("/", name="homepage")
+      */
+      public function home(){
+         return $this->render('home.html.twig', [
+            'page' => 'Home', 
+            'navigation'=>['href'=>'#', 'caption'=>"HOME"]
+         ]);
+         //return $this->render('BizLand/index.html', ['the' => 'variables', 'go' => 'here']);
+      }
+
+      /**
+       * @Route("/{slug}", name="homepage_slug")
+      */
+      public function show($slug=''){
+         return $this->render('home.html.twig', [
+            'page' => 'Home/'.$slug, 
+            'navigation'=>['href'=>'#', 'caption'=>"HOME"]
+         ]);
+         //return $this->render('BizLand/index.html', ['the' => 'variables', 'go' => 'here']);
+      }
+   }
